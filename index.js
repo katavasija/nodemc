f42efdda843bc1a9a3253eaffc0a40bd20a21c7c
katavasija <romanenkok81@gmail.com>
2021-12-31 01:16:46 +0700

    MC-002 request path

--- index.js
+++ index.js
@@ -5,10 +5,22 @@
  
  // dependencies
  const http = require('http');
+ const url = require('url');
  
  // The server should respond to all requests with a string.
  const server = http.createServer( function(req, res) {
+	// get the url and parse it
+	const parsedUrl = url.parse(req.url, true);
+	
+	// get the pathname
+	const path = parsedUrl.pathname;
+	const trimmedPath = path.replace(/^\/|\/+$/g, '');
+	
+	// send the response
 	res.end('Server is here)');
+	
+	// log
+	console.log('Request recieved on path:' + trimmedPath);
  });
   
  // Start the server and have it listen on port 3000
